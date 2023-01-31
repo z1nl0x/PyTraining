37671c0c3ec7e1e1aeaa583649fb2705da8edd72
Paulo Ernesto Kreft Margato <pmargato@tcontatto.corp>
2023-01-31 17:15:42 -0300

    Continuing the study

--- python_stuff.py
+++ python_stuff.py
@@ -40,3 +40,49 @@
 #     if status == 'active':
 #         active_users[user] = status
 # print(active_users)
+
+# for i in range(5):
+#     print(i)
+
+# print(list(range(5, 10)))
+
+# print(list(range(0, 10, 3)))
+
+# print(list(range(-10, -100, -30)))
+
+# a = ['teste1', 'teste2', 'teste3', 'teste4']
+
+# for i in range(len(a)):
+#     print(i, a[i])
+
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(n, 'igual', x, '*', n//x)
+#             break
+#     else:
+#         print(n, 'é número primo')
+
+# for num in range(2, 10):
+#     if num % 2 == 0:
+#         print(num, 'Número par encontrado!')
+#         continue
+#     print(num, 'Número impar encontrado!')
+
+####################################################
+# Match Statements
+####################################################
+
+# def http_error(status):
+#     match status:
+#         case 400:
+#             return print('Bad Request!')
+#         case 404:
+#             return print('Not found!')
+#         case 418:
+#             return print("I'm a teapot!")
+#         case _:
+#             return print('Algo deu errado com a internet!')
+
+# http_error(404)
+
