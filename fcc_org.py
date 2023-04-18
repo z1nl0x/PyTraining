2d949bcd03c05f9416b2631c2a2bab905a041948
Paulo Kreft <paulo.kreft@gmail.com>
2023-04-18 10:58:39 -0300

    Studied today

--- fcc_org.py
+++ fcc_org.py
@@ -1,1 +1,73 @@
-# Free Code Camp - Code Examples
+# Fee Code Camp - Code Examples
+
+# print('Hello World!')
+
+# Math Expresions
+
+#x = 1 + 2 ** 3 / 4 * 5
+#print(x)
+
+#print(float(99) + 100)
+
+#i = 42
+
+#type(i)
+
+#f = float(i)
+#print(f)
+#print(type(f))
+
+# Integer division produces a floating point result
+
+#print(9 / 2)
+
+#print(99 / 100)
+
+#print(10.0 / 2.0)
+
+
+# String conversions
+
+#sval = '123'
+
+#print(type(sval))
+
+#print(int(sval) + 1)
+
+# User Input
+
+#nam = input('Who are you? ')
+#print('Welcome', nam)
+
+#x = 20
+
+#if x < 2:
+#    print('Small')
+#elif x < 10:
+#    print('Medium')
+#else:
+#    print('Large')
+#print('All Done!')
+
+
+astr = 'Hello Kreft'
+try:
+    istr = int(astr)
+except:
+    istr = -1
+print('First', istr)
+
+astr = '123'
+
+try:
+    istr = int(astr)
+except:
+    istr = -1
+print('Second', istr)
+
+ 
+
+
+
+
+
