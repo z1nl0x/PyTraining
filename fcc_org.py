34836fb88c53bbccc66b78689f796d0b4875fa0a
z1nl0x <paulo.kreft@gmail.com>
2023-05-04 17:12:21 -0300

    Studied 04-05-2023 17:12:00

--- fcc_org.py
+++ fcc_org.py
@@ -100,7 +100,45 @@
 #print('Done!')
 
 
+#largest_so_far = -1
+#print('Before', largest_so_far)
+#for the_num in [9, 41, 12, 3, 74, 15]:
+#    if the_num > largest_so_far:
+#        largest_so_far = the_num
+#    print(largest_so_far, the_num)
+#
+#print('After', largest_so_far)
 
+
+#zork = 0
+
+#print('Before', zork)
+#for thing in [9, 41, 12, 3, 74, 15]:
+#    zork = zork + 1
+#    print(zork, thing)
+#print('After', zork) 
+
+
+#found = False
+#print('Before', found)
+#for value in [9, 41, 12, 3, 74, 15]:
+#    if value == 3:
+#        found = True
+#    print(found, value)
+#print('After', found)
+
+
+smallest = None
+print('Before')
+for value in [9, 41, 12, 3, 74, 15]:
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest = value
+    print(smallest, value)
+print('After', smallest)
+
+          
 
 
 
